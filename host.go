71ee6e049d96ef994f2eef634fa9b97748fb4e6c
Elliott Maguire <glotchimo@pm.me>
2022-11-08 18:55:32 -0800

    Change host -> up

--- host.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"io"
-)
-
-func hostUsage(w io.Writer) {}
-
-func hostCmd(args []string) {}
---
+++ up.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"io"
+)
+
+func upUsage(w io.Writer) {}
+
+func upCmd(args []string) {}
